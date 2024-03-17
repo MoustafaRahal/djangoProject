e32f65663f442904375c5ec22ef9e24597cbf060
Moustafa Rahal <mosta.rahalg@gmail.com>
2024-03-17 16:10:45 +0000

    Login auth done and tested

--- accountsApp/urls.py
+++ accountsApp/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('update_order/<str:pk>', views.updateOrder, name='update_order'),
     path('delete_order/<str:pk>', views.deleteOrder, name='delete_order'),
     path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerPage, name='register'),
 ]
--- accountsApp/views.py
+++ accountsApp/views.py
@@ -6,8 +6,11 @@ from django.forms import inlineformset_factory
 from .filter import OrderFilter
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
+
 
 def registerPage(request):
     form = CreateUserForm()
@@ -22,10 +25,25 @@ def registerPage(request):
     context = {'form':form}
     return render(request, './register.html', context)
 
-
 def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Invalid Username or Password')
+            return redirect('login')
+
     context ={}
     return render(request, './login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
 
 def home(request):
     orders = Order.objects.all()
